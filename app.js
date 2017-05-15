0b4aaa155e17ffa564e97961188eb301a58784f8
Madeleine Johnson <mjohnson3038@gmail.com>
2017-05-14 17:48:31 -0700

    home section complete

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var app = angular.module('myApp', []);
 
 app.controller('WebsiteController', function($scope) {
-    $scope.test ="Hi Maddie!";
-
-    $scope.navbarCollapsed = true;
+    this.test ="Hi Maddie!";
 
 
   });
